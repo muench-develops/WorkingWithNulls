acbbfd1b69191186aff644b6c64242880c8faee5
Justin Münch <justin.muench@iubh-fernstudium.de>
2020-07-15 07:46:00 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Working with Null Values
+        }
+    }
+}
